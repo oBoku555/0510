7cf6b71e0870e9b5f3be38e27b9936969b0e904f
oboku555 <aa40706aa@gmail.com>
2024-05-31 16:34:36 +0800

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/components/Card.jsx 	modified:   src/app/page.js

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,11 @@
+/*
+* @author 蘇新宇 <gmail.com>
+*/
+export default function CustomCard({item}){
+    return(
+        <div className="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    )
+
+}
